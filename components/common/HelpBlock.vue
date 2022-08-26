a78e658d3d4336dbf363fb1c10c1989addb172e4
wanted6610 <vatocat_nn@mail.ru>
2022-08-26 21:06:52 +0300

    правки

---
+++ components/common/HelpBlock.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "HelpBlock"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/pages/home/OfferSwiper.vue
@@ -0,0 +1,28 @@
+<template>
+  <hooper>
+    <slide v-for="(slide, index) in slides" :key="slide.id">
+      <h3 class="slide__title">{{slide.title}}</h3>
+      <p class="slide__desc">{{slide.desc}}</p>
+    </slide>
+  </hooper>
+</template>
+
+<script>
+import { Hooper, Slide } from 'hooper';
+import 'hooper/dist/hooper.css';
+
+export default {
+  name: "BaseSwiper",
+  components: {Hooper, Slide},
+  props: {
+    slides: {
+      type: Array,
+      default: () => [],
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
